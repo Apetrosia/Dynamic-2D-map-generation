cefebb919ee4f6dd3ace082246dbe7e247ea409f
Анна Петросян <apetrosia@sfedu.ru>
2025-03-10 01:18:27 +0300

    Add class chunk

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public int leftChunk = -1;
+    public int rightChunk = -1;
+    public int topChunk = -1;
+    public int bottomChunk = -1;
+    public int offsetX;
+    public int offsetY;
+}
